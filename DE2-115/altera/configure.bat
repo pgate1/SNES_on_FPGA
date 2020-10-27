190bc8ef44aefb115e92ef877106e70dcbd9b179
pgate1 <pgate.one@gmail.com>
2020-10-27 15:55:20 +0900

    support BigROM

---
+++ DE2-115/altera/configure.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set PATH=%QUARTUS_ROOTDIR%\bin64;%QUARTUS_ROOTDIR%\bin32;%QUARTUS_ROOTDIR%\bin;%PATH%
+
+quartus_pgm.exe -c "USB-Blaster" -m JTAG -o p;output_files\SNES.sof
+
+echo Press any key to finish.
+pause > nul
+
+exit
